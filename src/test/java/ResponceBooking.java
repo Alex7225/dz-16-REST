a4523f08fb0295dbcc4cfbd2dc0c3adae2d5a0bb
oleksandr <oleksandr_grigor@ukr.net>
2023-07-20 22:46:56 +0100

    first commit

---
+++ src/test/java/ResponceBooking.java
@@ -0,0 +1,12 @@
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class ResponceBooking {
+    private int bookingid;
+    //private CreateBooking booking;
+}
